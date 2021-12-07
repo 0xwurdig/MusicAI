19a37d13c81a065338ed6bf3cff4f66043280647
wurdig-mich <harshay.alpha@gmail.com>
2021-12-07 16:55:30 +0530

    final

---
+++ hard_update.py
@@ -0,0 +1,5 @@
+import Update_Model
+
+# emotions = ["angry", "happy", "sad", "neutral", "fear", "surprise", "disgust"]
+emotions = ["fear", "surprise", "disgust", "angry", "happy", "sad", "neutral"]
+Update_Model.update(emotions)
